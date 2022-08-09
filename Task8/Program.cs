b9565a2706a5bb3dc495b92851089950cf8f20b5
Natahis <natahis@bk.ru>
2022-08-09 20:29:00 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.WriteLine("Введите одно целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 2) Console.WriteLine($"Нет чётных чисел в диапазоне от 1 до введенного числа.");
+
+int count = 2;
+while (num >= count)
+{
+    Console.Write($"{count} ");
+    count = count + 2;
+}
+Console.WriteLine();
